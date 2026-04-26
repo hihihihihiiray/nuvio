e7a2409913a8144ee5030e91367e19b8ce3dba33
raymondnoodles <raymondvu879@gmail.com>
2026-04-26 14:15:16 -0500

    Update animekai.js

--- providers/animekai.js
+++ providers/animekai.js
@@ -6,7 +6,7 @@ const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const ANILIST_URL = 'https://graphql.anilist.co';
 
 var HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
     'Connection': 'keep-alive',
     'Referer': 'https://animekai.to/'
 };
